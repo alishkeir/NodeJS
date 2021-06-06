c739744eff6f49e939cbe2dcb23c6a586d34fc33
alishkeir <alishkeir_@hotmail.com>
2021-06-06 12:11:24 +0300

    modules

---
+++ 4.Modules/app.js
@@ -0,0 +1,13 @@
+const { sum, sub, div, mul } = require("./myModules");
+
+
+const mySum = sum(10, 20);
+const mySub = sub(10, 20);
+const myMul = mul(10, 20);
+const myDiv = div(10, 20);
+
+console.log(mySum);
+console.log(mySub);
+console.log(myMul);
+console.log(myDiv);
+
